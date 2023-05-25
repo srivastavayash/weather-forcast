e8bda3f889306c2b051e02d1a90d64f890fc4366
Srivastavayash <srivastavayash017@gmail.com>
2023-05-25 17:08:36 +0530

    minor changes

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -9,7 +9,7 @@ const Weather = () => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${q}`
       const response = await fetch(url);
       const resjson = await response.json();
-      // console.log(resjson)
+      console.log(resjson); //changing complete ui design 
       setCity(resjson.main);
     };
     fetchApi();
@@ -21,7 +21,7 @@ const Weather = () => {
           setSearch(event.target.value)
         }} />
       </div>
-      {!city ? (<div className='errormsg'><h1><b> Data Not Found !</b></h1></div>) : (<div className='info'>
+      {!city ? (<div className='errormsg'><h1><b>data not found !</b></h1></div>) : (<div className='info'>
         <h2>
           <i className="fa-solid fa-location-dot"></i> {search}
         </h2>
