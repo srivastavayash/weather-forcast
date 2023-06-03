5b4c05654f76c4e3a186ed142aa3bc36843d9ba6
Srivastavayash <srivastavayash017@gmail.com>
2023-06-03 17:18:57 +0530

    Added time meridian for data in cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './Card.css'
 const Card = (props) => {
+  const std = props.data;
+  let [val, setVal] = useState(null);
+  useEffect(() => {
+    const fun = async () => {
+
+      if (std[0] === "0") {
+        setVal("AM");
+      }
+      else {
+        setVal("PM");
+      }
+    };
+    fun();
+  }, [std])
+
   return (
-    <div className='single-card'><h3>{props.data} {props.std}</h3>
+    <div className='single-card'><h3>{props.data} {val} </h3>
     </div>
   )
 }
